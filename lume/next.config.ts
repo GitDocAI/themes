5eadc5636a47e9f2f5428d2ec29f5b41792bb1d4
AlexSanchez-bit <sanchez.saez.alex01@gmail.com>
2025-09-22 21:25:04 -0400

    feat(templates): waiting for static data generation before deploying website

--- lume/next.config.ts
+++ lume/next.config.ts
@@ -1,28 +1,32 @@
 import nextra from 'nextra';
 import fs from 'fs';
 import path from 'path';
-import {init} from './shared/file_indexer/init_search_engine'
-import {openapi_path_builder} from './shared/path_builder'
+import { init } from './shared/file_indexer/init_search_engine';
+import { openapi_path_builder } from './shared/path_builder';
 
 const configPath = path.join(process.cwd(), 'gitdocai.config.json');
-
-let siteConfig = {};
-init().then(_=>{
-})
-
-try {
-  const raw = fs.readFileSync(configPath, 'utf-8');
-  siteConfig = openapi_path_builder(JSON.parse(raw));
-} catch (err) {
-  console.warn('Error reading gitdocai.config.json');
-}
 
 const withNextra = nextra({
   staticImage: true,
 });
 
-export default withNextra({
-  env: {
-    SITE_CONFIG: JSON.stringify(siteConfig),
-  },
-});
+export default async function config() {
+  await init();
+
+  let siteConfig = {};
+  try {
+    const raw = fs.readFileSync(configPath, 'utf-8');
+    siteConfig = openapi_path_builder(JSON.parse(raw));
+  } catch (err) {
+    console.warn('Error reading gitdocai.config.json');
+  }
+
+  return withNextra({
+    env: {
+      SITE_CONFIG: JSON.stringify(siteConfig),
+    },
+  });
+}
+
+
+
