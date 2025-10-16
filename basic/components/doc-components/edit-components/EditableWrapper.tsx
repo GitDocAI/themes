a1186cb92c14667b64ad490be48c3470ec2dc874
AlexSanchez-bit <sanchez.saez.alex01@gmail.com>
2025-10-16 12:09:53 -0400

    fix[basic](edition): setted edition wrapper to a server component

--- basic/components/doc-components/edit-components/EditableWrapper.tsx
+++ basic/components/doc-components/edit-components/EditableWrapper.tsx
@@ -1,7 +1,4 @@
-'use client'
-
 import React from 'react'
-import { usePathname } from 'next/navigation'
 import { EditableContent } from './EditableContent'
 
 const is_prod = process.env.NODE_ENV == 'production'
@@ -9,16 +6,14 @@ const webhook_url = process.env.WEBHOOK_URL
 const authentication = process.env.AUTHENTICATION
 
 export const EditableWrapper = ({children,original_content}:{children:any,original_content?:string})=>{
-  const pathname = usePathname()
-
   return is_prod ? (<>{children}</>) : (
     <EditableContent
       originalMarkdown={original_content || ''}
       webhook_url={webhook_url || ''}
-      authentication={authentication || ''}
-      file_path={pathname + '.mdx'}>
+      authentication={authentication || ''}>
       {children}
     </EditableContent>
   )
 }
+
 
