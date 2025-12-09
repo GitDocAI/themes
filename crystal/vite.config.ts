a33493b3ac935045199ba35373f8b8aaf2daa737
AlexSanchez-bit <sanchez.saez.alex01@gmail.com>
2025-12-09 08:35:24 -0500

    rm[crystal](vector-search): removed vectorization plugin

--- crystal/vite.config.ts
+++ crystal/vite.config.ts
@@ -1,14 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import vectorSearchPlugin from './vite-plugin-vector-search'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode`
-  const env = loadEnv(mode, process.cwd(), '')
-
+export default defineConfig(() => {
   return {
-    plugins: [react(), vectorSearchPlugin(env.VITE_MODE),tailwindcss()],
+    plugins: [react(),tailwindcss()],
   }
 })
